8f806889008297c0fd316e0f94239d24b0947abe
Caio Ramos <aramoscaio@gmail.com>
2023-05-07 22:21:06 -0300

    feat: added top framm and bottom frame

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from tkinter import *
 
 
+
 root = Tk()
 
 class Application():
     def __init__(self):
         self.root = root 
         self.screen_config()
+        self.screen_frame()
         root.mainloop()
 
     def screen_config(self):
@@ -16,6 +18,16 @@ class Application():
         self.root.resizable(True, True)
         self.root.maxsize(width=900, height=700)
         self.root.minsize(width=400, height=300)
+    
+     
+    def screen_frame(self):
+        self.top_frame = Frame(self.root, bd=4, bg="#dfe3ee", 
+        highlightbackground="#759fe6", highlightthickness=3)
+        self.top_frame.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
+
+        self.bottom_frame = Frame(self.root, bd=4, bg="#dfe3ee", 
+        highlightbackground="#759fe6", highlightthickness=3)
+        self.bottom_frame.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
 Application()
 
 
